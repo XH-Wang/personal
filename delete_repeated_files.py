0be1a56b0ae2f7600f6262db2037dedb179e8a30
wxhown <wxhown@qq.com>
2020-06-14 13:23:16 +0800

    1.0版本发布

--- delete_repeated_files.py
+++ delete_repeated_files.py
@@ -2,46 +2,48 @@
 import os
 import filecmp
 
-# 将指定目录下的所有文件的路径存储到all_files变量中
-def get_all_files(path, dirs):
-    all_files = []
-    for d in dirs:
-        cur_path = os.path.join(path, d)
-        files = os.listdir(cur_path)
-        for f in files:
-            all_files.append(os.path.join(cur_path, f))
-    return all_files
-
-# 比较两个文件的内容是否一致
-def cmp_files(x, y):
-    if filecmp.cmp(x, y):
-        # 如果一致，则删除第二个，保留第一个，并输出信息
-        os.remove(y)
-        print("路径\"" + y + "\"下的文件是重复文件，已经删除")
-
-
 # 输入路径
 path = input("Please Enter Path:") 
 
 # 遍历所有文件，获取有用文件列表及重复文件列表
+file_ls_keep = []
+file_ls_rept = []
 
+for root, dirs, files in os.walk(path):
+    # 遍历文件
+    for f in files:
+        cur_path = os.path.join(root, f)
+
+        if len(file_ls_keep) == 0:
+            file_ls_keep.append(cur_path)
+            continue
+
+        for file_keep in file_ls_keep:
+            if filecmp.cmp(file_keep, cur_path):
+               file_ls_rept.append(cur_path) 
+               break
+
+            if file_keep == file_ls_keep[-1]:
+                file_ls_keep.append(cur_path)
+                break
 
 # 输出重复文件列表，确认是否删除
+print("keep_list")
+for f in file_ls_keep:
+    print(f)
 
+print("repeated_list")
+for f in file_ls_rept:
+    print(f)
+
+option = input("Delete all repeated files?[Y/N]") 
 
 # 若选择删除则删掉重复文件列表中的所有文件
+if option == "Y":
+    for f in file_ls_rept:
+        os.remove(f)
+        print("Repeated file:\"" + f + "\" deleted")
+else:
+    print("All file keep.")
 
-
-
-
-# 已知路径下存在两个文件夹pic1和pic2
-dirs = ['pic1', 'pic2']
-# 调用函数，获取文件列表
-all_files = get_all_files(path, dirs)
-# 用双重for循环来比较文件是否有重复
-for x in all_files:
-    for y in all_files:
-        # 如果x和y不是相同的文件，而且都存在，则执行后续操作
-        if x != y and os.path.exists(x) and os.path.exists(y):
-            # 比较两个文件的内容是否一致
-           cmp_files(x,y)
+print("completed.")
